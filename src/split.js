beefdb0d121908bfcbe9a638bc9516623c077938
Scott Christopher <scott.christopher@hiveempire.com>
2015-11-12 14:39:18 +1100

    Fixed split

--- src/split.js
+++ src/split.js
@@ -1,14 +1,10 @@
 module.exports = function split(char, str) {
-  if (str.length === 0) {
-    return [];
+  var idx = str.indexOf(char);
+  if (idx === -1) {
+    return [str];
   } else {
-    var idx = str.indexOf(char);
-    if (idx === -1) {
-      return [str];
-    } else {
-      var firstPart = str.slice(0, idx);
-      var rest = str.slice(idx + 1);
-      return [firstPart].concat(rest.length === 0 ? [] : split(char, rest));
-    }
+    var firstPart = str.slice(0, idx);
+    var rest = str.slice(idx);
+    return [firstPart].concat(rest.length === 0 ? [] : split(char, rest.slice(1)));
   }
 };
