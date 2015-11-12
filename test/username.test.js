fe1dbba47d594c34eaf0b769c4df353f56f7194b
Scott Christopher <scott.christopher@hiveempire.com>
2015-11-12 14:46:53 +1100

    Added prop tests for validate

--- test/username.test.js
+++ test/username.test.js
@@ -1,4 +1,36 @@
+var jsv = require('jsverify');
+var validate = require('../src/validate');
+
+
 describe('validate', function() {
-  it('ensures a username contains at least 8 characters');
-  it('ensures a username contains at least one lowercase, one uppercase and one numeric character');
+
+  var lower = jsv.elements('abcdefghijklmnopqrstuvwxyz'.split(''));
+  var upper = jsv.elements('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''));
+  var digits = jsv.elements('0123456789'.split(''));
+
+  var proper = jsv.tuple(
+    [lower, upper, digits, lower, upper, digits, lower, upper]
+  ).smap(function(charsArray) {
+    return charsArray.join('');
+  }, function(str) {
+    return str.split('');
+  });
+
+  var tooShort = proper.smap(function(username) {
+    return username.slice(-5);
+  }, function(username) {
+    return username;
+  });
+
+  it('ensures a username contains at least 8 characters', function() {
+    jsv.assert(jsv.forall(tooShort, function(username) {
+      return !validate(username);
+    }));
+  });
+
+  it('ensures a username contains at least one lowercase, one uppercase and one numeric character', function() {
+    jsv.assert(jsv.forall(proper, function(username) {
+      return validate(username);
+    }));
+  });
 });
