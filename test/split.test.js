47fa8cfc125a095fbdc9a8ec0150f19f80ddb990
Scott Christopher <scott.christopher@hiveempire.com>
2015-11-12 13:54:18 +1100

    Added test stubs

---
+++ test/split.test.js
@@ -0,0 +1,3 @@
+describe('split', function() {
+  it('breaks a string into substrings delimited by a given character');
+});
---
+++ test/username.test.js
@@ -0,0 +1,4 @@
+describe('validate', function() {
+  it('ensures a username contains at least 8 characters');
+  it('ensures a username contains at least one lowercase, one uppercase and one numeric character');
+});
