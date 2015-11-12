62961a403636e444a878bf6539b7b30b581120ab
Scott Christopher <scott.christopher@hiveempire.com>
2015-11-12 14:22:52 +1100

    Added prop test for split

--- test/split.test.js
+++ test/split.test.js
@@ -1,10 +1,21 @@
 var assert = require('assert');
 var split = require('../src/split');
+var jsv = require('jsverify');
 
 
 describe('split', function() {
+  function unsplit(char, parts) {
+    return parts.join(char);
+  }
+
   it('breaks a string into substrings delimited by a given character', function() {
     assert.deepEqual(split('@', 'scott@finder.com'), ['scott', 'finder.com']);
     assert.deepEqual(split('/', '/Users/scott'), ['', 'Users', 'scott']);
+
+    jsv.assert(jsv.forall(jsv.nestring, function(str) {
+      return jsv.forall(jsv.elements(str.split('')), function(char) {
+        return unsplit(char, split(char, str)) === str;
+      });
+    }));
   });
 });
