5fb58d0a35da5e857c9e773863a5fac029a6c728
Scott Christopher <scott.christopher@hiveempire.com>
2015-11-12 14:09:45 +1100

    Added split tests

--- test/split.test.js
+++ test/split.test.js
@@ -1,3 +1,10 @@
+var assert = require('assert');
+var split = require('../src/split');
+
+
 describe('split', function() {
-  it('breaks a string into substrings delimited by a given character');
+  it('breaks a string into substrings delimited by a given character', function() {
+    assert.deepEqual(split('@', 'scott@finder.com'), ['scott', 'finder.com']);
+    assert.deepEqual(split('/', '/Users/scott'), ['', 'Users', 'scott']);
+  });
 });
